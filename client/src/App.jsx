f5a78d8794b061d07782c047bf1b3cfb958921e7
mnrclk07 <mnrclk21@gmail.com>
2024-09-18 21:43:27 +0300

    create hader component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
@@ -16,6 +18,7 @@ function App() {
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/profile' element={<Profile />} />
       </Routes>
+
     </BrowserRouter>
   )
 }
