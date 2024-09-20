813fffa85e005a188a2fb0fc8829be5d7414e040
mnrclk07 <mnrclk21@gmail.com>
2024-09-20 18:04:03 +0300

    create sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import exspress from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, signin } from "../controllers/auth.controller.js";
 
 const router = exspress.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
