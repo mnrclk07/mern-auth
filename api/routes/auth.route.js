f40eadfe095970d9edd8e1167a1882c92f854f0e
mnrclk07 <mnrclk21@gmail.com>
2024-09-27 15:23:04 +0300

    add sign out user functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,16 @@
 import exspress from "express";
-import { signup, signin, google } from "../controllers/auth.controller.js";
+import {
+  signup,
+  signin,
+  google,
+  signout,
+} from "../controllers/auth.controller.js";
 
 const router = exspress.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/google", google);
+router.get("/signout", signout);
 
 export default router;
